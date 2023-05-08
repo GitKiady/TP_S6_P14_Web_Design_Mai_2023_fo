ebc7a1e69e32585b7d87d08a58f9f83d4dcca979
GitKiady <102509566+GitKiady@users.noreply.github.com>
2023-05-08 19:06:17 +0300

    Update acceuil.blade.php

--- resources/views/content/acceuil.blade.php
+++ resources/views/content/acceuil.blade.php
@@ -7,7 +7,7 @@
 @extends('../layout/layout')
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=10, minimum-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="format-detection" content="telephone=no">
     <meta name="description" content="L'intelligence artificielle est en vogue actuellement donc ne raté aucune nouvelle" >
