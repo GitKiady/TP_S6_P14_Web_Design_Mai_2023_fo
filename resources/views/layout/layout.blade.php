e7404f01b1a3e2a90d673569d7aa5f2d410025eb
GitKiady <102509566+GitKiady@users.noreply.github.com>
2023-05-08 16:39:56 +0300

    Update layout.blade.php

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,11 +1,11 @@
-	<link rel="stylesheet" href="css/fancySelect.css" />
-	<link rel="stylesheet" href="css/uniform.css" />
-	<link rel="stylesheet" href="css/all.css" />
-	<link media="screen" rel="stylesheet" type="text/css" href="css/screen.css" />
+	<link rel="stylesheet" href="/css/fancySelect.css" />
+	<link rel="stylesheet" href="/css/uniform.css" />
+	<link rel="stylesheet" href="/css/all.css" />
+	<link media="screen" rel="stylesheet" type="text/css" href="/css/screen.css" />
 
-	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-	<script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
-	<script type="text/javascript" src="js/jquery.placeholder.js"></script>
-	<script type="text/javascript" src="js/jquery.uniform.min.js"></script>
-	<script type="text/javascript" src="js/fancySelect.js"></script>
-	<script type="text/javascript" src="js/main.js"></script>
+	<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="/js/jquery.bxslider.min.js"></script>
+	<script type="text/javascript" src="/js/jquery.placeholder.js"></script>
+	<script type="text/javascript" src="/js/jquery.uniform.min.js"></script>
+	<script type="text/javascript" src="/js/fancySelect.js"></script>
+	<script type="text/javascript" src="/js/main.js"></script>
